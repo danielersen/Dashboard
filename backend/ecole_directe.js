9f4d617786cf6879e557366d54730c9c3f2e5efd
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-05-24 20:15:57 +0200

    Update ecole_directe.js

--- backend/ecole_directe.js
+++ backend/ecole_directe.js
@@ -1,4 +1,4 @@
-export function handleED(user, password, category) {
+export async function handleED(user, password, category) {
   const userAgent =
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36";
 
