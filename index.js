63ef6efab2c7d671ad82c8dc986b6753d8bf2cfd
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-05-24 16:38:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-import { handleAPI } from "./backend/API/app.js"
-import { handleAPI_ws } from "./backend/Server/node/client_example.js"
-import { handleWebSocket } from "./backend/Server/node/server_example.js"
+import { handleED } from "./backend/ecole_directe.js"
 
 export default {
   async fetch(request, env, ctx) {
@@ -16,13 +14,15 @@ export default {
     if (env.MODE !== "prodution") {
       return new Response("Not Found", { status: 404 })
     }
-      
+
     // =========================
     // 🌐 SITE (Cloudflare assets)
     // =========================
     if (
-      url.pathname === "/" ||
-      url.pathname.startsWith("/assets")
+      (url.pathname === "/" ||
+      url.pathname === "" ||
+      url.pathname.startsWith("/assets"))&&
+      SITE === "production"
     ) {
       return env.ASSETS.fetch(request)
     }
@@ -30,22 +30,10 @@ export default {
     // =========================
     // 📶 MAIN API
     // =========================
-    if (url.pathname.startsWith("/api")) {
-      return handleAPI(request, env, ctx, secretJson)
-    }
-
-    // =========================
-    // 📡 WS API
-    // =========================
-    if (url.pathname.startsWith("/api-ws")) {
-      return handleAPI_ws(request, env, ctx, secretJson)
-    }
-
-    // =========================
-    // 🔌 WEBSOCKET
-    // =========================
-    if (url.pathname === "/ws") {
-      return handleWebSocket(request, env, ctx, secretJson)
+    
+    // Ecoledirecte handle
+    if (url.pathname.startsWith("/api/ed")) {
+      return handleED(request, env, ctx, secretJson)
     }
 
     // =========================
