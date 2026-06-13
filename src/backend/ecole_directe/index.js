060b606744ddbe36c02ec850a2bf90b00149b813
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-06-13 13:33:14 +0000

    Update index.js

--- src/backend/ecole_directe/index.js
+++ src/backend/ecole_directe/index.js
@@ -4,26 +4,27 @@ import { EDhomeworks } from "./homeworks.js"
 import { EDtimetable } from "./timetable.js"
 
 export async function EDfunction (env, subpath, headers) {
-  const informations = await EDinformations(env)
-  if (subpath === "info") {
-    return informations
-  }
-
-  // Cases with filter
   let filter;
   if (headers.get("filter") === "true") {
     filter = true;
   } else {
     filter = false;
   }
+  const informations = await EDinformations(env)
+  
+  if (subpath === "info") {
+    return informations
+  }
   if (subpath === "grades") {
     return await EDgrades (env, informations, filter)
+  } else if (subpath === "averages") {
+    return await EDaverages (await EDgrades (env, informations, filter))
+  } else if (subpath === "new-grades") {
+    return await EDnewgrades (await EDgrades (env, informations, filter))
   } else if (subpath === "homeworks") {
     return await EDhomeworks (env, informations, filter)
   } else if (subpath === "timetable") {
     return await EDtimetable (env, informations, filter)
-  } else if (subpath === "") {
-    return ""
   } else {
     return "There is no ed features which match with your request"
   }
