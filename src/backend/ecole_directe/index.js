2f196ee09d2566f5cf6921a9268bd353f3662e62
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-06-14 23:29:49 +0200

    Update index.js

--- src/backend/ecole_directe/index.js
+++ src/backend/ecole_directe/index.js
@@ -17,18 +17,45 @@ export async function EDfunction (env, subpath, method, headers, body) {
   
   if (subpath === "info" && method === "GET") {
     return informations
-  } else if (subpath === "grades" && method === "GET") {
+    
+  } else if (
+    subpath === "grades" && 
+    method === "GET"
+  ) {
     return await EDgrades (env, informations, filter)
-  } else if (subpath === "averages" && method === "GET") {
+  
+  } else if (
+    subpath === "averages" && 
+    method === "GET"
+  ) {
     return await EDaverages (await EDgrades (env, informations, true))
-  } else if (subpath === "new-grades" && method === "GET") {
+  
+  } else if (
+    subpath === "new-grades" && 
+    method === "GET"
+  ) {
     return await EDnewgrades (await EDgrades (env, informations, true))
-  } else if (subpath === "homeworks" && method === "GET") {
+  
+  } else if (
+    subpath === "homeworks" && 
+    method === "GET"
+  ) {
     return await EDhomeworks (env, informations, filter)
-  } else if (subpath === "homeworks" && method === "POST" && /^\d{5}$/.test(body?.id)) {
-    return await EDhomeworksDone (env, informations, filter, body?.id)
-  } else if (subpath === "timetable" && method === "GET") {
+  
+  } else if (
+    subpath === "homeworks" && 
+    method === "POST" && 
+    /^\d{5}$/.test(body?.id) &&
+    ["true", "false"].includes(body?.done)
+  ) {
+    return await EDhomeworksDone (env, informations, body?.id, body?.done)
+  
+  } else if (
+    subpath === "timetable" && 
+    method === "GET"
+  ) {
     return await EDtimetable (env, informations, filter)
+  
   } else {
     return "There is no ed features matching with your request"
   }
