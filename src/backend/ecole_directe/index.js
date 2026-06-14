bc20c782f86b0eccfb69bda19a0d55a1033150a6
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-06-14 21:14:10 +0200

    Update index.js

--- src/backend/ecole_directe/index.js
+++ src/backend/ecole_directe/index.js
@@ -3,9 +3,10 @@ import { EDgrades } from "./grades.js"
 import { EDaverages } from "./grades.js"
 import { EDnewgrades } from "./grades.js"
 import { EDhomeworks } from "./homeworks.js"
+import { EDhomeworksDone } from "./homeworks.js"
 import { EDtimetable } from "./timetable.js"
 
-export async function EDfunction (env, subpath, headers) {
+export async function EDfunction (env, subpath, method, headers) {
   let filter;
   if (headers.get("filter") === "true") {
     filter = true;
@@ -17,15 +18,17 @@ export async function EDfunction (env, subpath, headers) {
   if (subpath === "info") {
     return informations
   }
-  if (subpath === "grades") {
+  if (subpath === "grades" && method === "GET") {
     return await EDgrades (env, informations, filter)
-  } else if (subpath === "averages") {
+  } else if (subpath === "averages" && method === "GET") {
     return await EDaverages (await EDgrades (env, informations, true))
-  } else if (subpath === "new-grades") {
+  } else if (subpath === "new-grades" && method === "GET") {
     return await EDnewgrades (await EDgrades (env, informations, true))
-  } else if (subpath === "homeworks") {
+  } else if (subpath === "homeworks" && method === "GET") {
     return await EDhomeworks (env, informations, filter)
-  } else if (subpath === "timetable") {
+  } else if (subpath === "timetable" && method === "POST") {
+    return await EDhomeworksDone (env, informations, filter)
+  } else if (subpath === "timetable" && method === "GET") {
     return await EDtimetable (env, informations, filter)
   } else {
     return "There is no ed features which match with your request"
