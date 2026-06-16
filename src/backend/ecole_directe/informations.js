00641e1c790c2af124e7c4a0a7c30fe2885014e1
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-06-16 23:27:32 +0200

    Update informations.js

--- src/backend/ecole_directe/informations.js
+++ src/backend/ecole_directe/informations.js
@@ -4,7 +4,7 @@ import { getCacheValue } from "../../index.js";
 export async function EDinformations(env, new_token) {
   // Answering the informations if a correct and actual response is saved
   if (
-    Math.floor(Date.now() / 60000) - getCacheValue("edTokenTime") &&
+    (Math.floor(Date.now() / 60000) - getCacheValue("edTokenTime")) < 29 &&
     new_token !== true
   ) {
     return getCacheValue("edTokenTime")
