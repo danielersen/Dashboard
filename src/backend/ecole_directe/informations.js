34d931ed7c6db26db6b9d30717fecf670dfeeb8b
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-06-16 21:39:28 +0200

    Update informations.js

--- src/backend/ecole_directe/informations.js
+++ src/backend/ecole_directe/informations.js
@@ -7,7 +7,6 @@ export async function EDinformations(env, new_token) {
     Math.floor(Date.now() / 60000) - getCacheValue("edTokenTime") &&
     new_token === false
   ) {
-    return "cache use"
     return getCacheValue(edToken)
   };
   
