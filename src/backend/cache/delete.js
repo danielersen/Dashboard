f3dd221fb493812aa192bd32b99cbd09f9b95a84
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-06-18 22:56:01 +0200

    Update delete.js

--- src/backend/cache/delete.js
+++ src/backend/cache/delete.js
@@ -2,3 +2,6 @@ export async function deleteCacheValue(key) {
   const req = new Request("https://cache/" + key);
   return await cache.delete(req);
 }
+export async function deleteCache() {
+  const req = new Request("https://cache/" + key);
+  return await cache.delete(req);
