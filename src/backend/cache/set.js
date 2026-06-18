80d2ec49a21c18d491777b356d8e6bc0b90648d6
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-06-18 23:13:18 +0200

    Update set.js

--- src/backend/cache/set.js
+++ src/backend/cache/set.js
@@ -1,31 +1,26 @@
 import { getCacheValue } from "./get.js"
-export async function setCacheValue(key, value, ttl = 1800) {
+async function setValue (functionKey, functionValue, ttl = 1800) {
+  const req = new Request("https://cache/" + functionKey);
+  const res = new Response(JSON.stringify(functionValue), {
+    headers: {
+      "Cache-Control": `max-age=${ttl}`
+    }
+  });
+  await cache.put(req, res);
+}
+export async function setCacheValue(key, value) {
+  if (key === "cache_keys") {
+    return "Error, this key is forbidden"
+  }
   // Save all the keys
-  const req1 = new Request("https://cache/" + "cache_keys");
-  const res1 = await cache.match(req1);
-  let cache_keys;
-  if (!res1) {
+  let cache_keys = await getCacheValue("cache_keys");
+  if (!cache_keys) {
     cache_keys = ["cache_keys"];
   } else {
     cache_keys = res.json();
   }
-  cache_keys.push(key)
-  const req2 = new Request("https://cache/" + "cache_keys");
-  const res2 = new Response(JSON.stringify(cache_keys), {
-    headers: {
-      "Cache-Control": `max-age=${ttl}`
-    }
-  });
-  await cache.put(req2, res2);
+  cache_keys.push(key);
+  await setValue("cache_keys", cache_keys);
   // Set the value
-  if (key === "cache_keys") {
-    return "Error, this key is forbidden"
-  }
-  const req3 = new Request("https://cache/" + key);
-  const res3 = new Response(JSON.stringify(value), {
-    headers: {
-      "Cache-Control": `max-age=${ttl}`
-    }
-  });
-  await cache.put(req3, res3);
+  await setValue(key, value);
 }
