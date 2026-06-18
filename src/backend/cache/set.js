8f3f2763a645a43f8b7995b3bc44c63517221023
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-06-18 17:47:20 +0200

    Update set.js

--- src/backend/cache/set.js
+++ src/backend/cache/set.js
@@ -1,9 +1,27 @@
 export async function setCacheValue(key, value, ttl = 1800) {
-  const req = new Request("https://cache/" + key);
-  const res = new Response(JSON.stringify(value), {
+  // Save all the keys
+  const req1 = new Request("https://cache/" + "cache_keys");
+  const res1 = await cache.match(req1);
+  let cache_keys;
+  if (!res1) {
+    cache_keys = [];
+  } else {
+    cache_keys = res.json();
+  }
+  cache_keys.push(key)
+  const req2 = new Request("https://cache/" + "cache_keys");
+  const res2 = new Response(JSON.stringify(cache_keys), {
     headers: {
       "Cache-Control": `max-age=${ttl}`
     }
   });
-  await cache.put(req, res);
+  await cache.put(req2, res2);
+  // Set the value
+  const req3 = new Request("https://cache/" + key);
+  const res3 = new Response(JSON.stringify(value), {
+    headers: {
+      "Cache-Control": `max-age=${ttl}`
+    }
+  });
+  await cache.put(req3, res3);
 }
