639a7b2974b48849a4bb343ea4e033f8db60c96b
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-06-19 23:40:43 +0200

    Update index.js

--- src/backend/cache/index.js
+++ src/backend/cache/index.js
@@ -2,10 +2,16 @@ import { getCacheValue } from "./get.js"
 import { setCacheValue } from "./set.js"
 import { deleteCacheValue, deleteCache } from "./delete.js"
 
-export async function Cache(method, path) {
+export async function Cache(method, path, body) {
   let resp
   if (method === "GET") {
     resp = await getCacheValue(path)
   } else if (method === "PATCH") {
-    resp = delete deleteCacheValue(path)
+    resp = await setCacheValue(path, body.get("value"))
+  } else if (method === "DELETE" && path) {
+    resp = await deleteCacheValue(path)
+  } else if (method === "DELETE") {
+    resp = await deleteCache()
+  }
+  return resp
 }
