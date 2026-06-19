522ff280edce788a1cca65ad6b42b622a1a08dac
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-06-19 08:01:12 +0200

    Create index.js

---
+++ src/backend/cache/index.js
@@ -0,0 +1,10 @@
+import { getCacheValue } from "./get.js"
+import { setCacheValue } from "./set.js"
+import { deleteCacheValue, deleteCache } from "./delete.js"
+
+export async function Cache(method, path) {
+  let resp
+  if (method === "GET") {
+    resp = await getCacheValue(path)
+  } else if (method === "PATCH")
+}
