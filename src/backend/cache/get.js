39e012461d42be707a61db60241bab3dc2cf5e6a
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-06-20 17:19:12 +0200

    Update cache.match to caches.default.match

--- src/backend/cache/get.js
+++ src/backend/cache/get.js
@@ -1,6 +1,6 @@
 export async function getCacheValue(key) {
   const req = new Request("https://cache/" + key);
-  const res = await cache.match(req);
+  const res = await caches.default.match(req);
   if (!res) return null;
   return await res.json();
 }
