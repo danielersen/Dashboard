d150816814df8f3b25eab6d1fa96287c18a60479
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-07-08 13:38:36 +0300

    Google Search console

--- src/frontend/components/navbar/navbar.js
+++ src/frontend/components/navbar/navbar.js
@@ -419,7 +419,7 @@ const NAVBAR_TEMPLATE = `
     <div class="inner">
       <a class="brand" data-home-link href="/pages/home" aria-label="Aller à l'accueil">
         <span class="brand-icon-wrap">
-          <img class="brand-icon" src="/assets/icons/websitelogo.png" alt="Dashboard">
+          <img class="brand-icon" src="/assets/logo/square.png" alt="Dashboard">
         </span>
       </a>
 
