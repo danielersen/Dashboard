a7397e35183dd53c6ab5d23d13ab093918266a33
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-07-09 23:27:41 +0300

    MEGA acceleration

---
+++ src/backend/cache/index.js
@@ -0,0 +1,6 @@
+import { getCacheValue } from "./get.js"
+import { setCacheValue } from "./set.js"
+import { deleteCacheValue, deleteCache } from "./delete.js"
+
+export { getCacheValue, setCacheValue, deleteCacheValue, deleteCache }
+export default { getCacheValue, setCacheValue, deleteCacheValue, deleteCache }
