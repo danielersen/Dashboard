caf283f3c2599ff68e5d29db004cdb9de0eeb8ac
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-07-09 23:31:30 +0300

    Deploy error

--- src/backend/cache/index.js
+++ src/backend/cache/index.js
@@ -3,4 +3,8 @@ import { setCacheValue } from "./set.js"
 import { deleteCacheValue, deleteCache } from "./delete.js"
 
 export { getCacheValue, setCacheValue, deleteCacheValue, deleteCache }
+
+// Export pour compatibilité avec l'import existant
+export const Cache = { getCacheValue, setCacheValue, deleteCacheValue, deleteCache }
+
 export default { getCacheValue, setCacheValue, deleteCacheValue, deleteCache }
