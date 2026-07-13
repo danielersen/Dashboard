e3e0d56d1e0d4409f13d291c89fd70203bb14411
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-07-13 10:35:05 +0200

    Sorting units

--- src/backend/tools/index.js
+++ src/backend/tools/index.js
@@ -1,11 +1,8 @@
 import { converterHandler } from "./converter.js";
-import { executorHandler } from "./executor.js";
 
 export async function ToolsFunction(env, path, method, body) {
   if (path === 'convert') {
     return await converterHandler(env, path, method, body);
-  } else if (path === 'execute') {
-    return await executorHandler(env, path, method, body);
   } else {
     throw new Error('Invalid tools path');
   }
